947f2029d5bd0a7c5eabcb792e5ae0446af3e557
Win 8 <Win 8@Home>
2020-06-03 20:25:10 +0530

    commit

--- src/main/java/com/jbk/Controller/MainController.java
+++ src/main/java/com/jbk/Controller/MainController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 	
 	public Question getQuestion() {
+		
 		Question question=new Question();
 		question.setQuestion("Is java platform Independent");
 		question.setOption1("YES");
 		question.setOption2("No");
 		question.setCorrectAnswer("1");
-		 question.setExplaination("due to byte code...which is independet to operating system");
+		question.setExplaination("due to byte code...which is independet to operating system");
 		return question;
 	}
 
